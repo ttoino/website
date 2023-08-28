56e80cdcb8a1c3aab2052ceaf5d328fe990ce5b2
toino <joaoapereira21@hotmail.com>
2023-08-28 02:12:42 +0100

    Make icons in projects more readable

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -13,13 +13,15 @@
 <article
     class="relative flex flex-col overflow-hidden rounded-md bg-mantle p-2 md:p-4"
 >
-    <img
-        src={project.image}
-        alt=""
-        class="-m-2 mb-2 h-auto max-w-[unset] md:-m-4 md:mb-2"
-    />
+    {#if project.image}
+        <img
+            src={project.image}
+            alt=""
+            class="-m-2 mb-2 aspect-video h-auto max-w-[unset] object-cover shadow-inner md:-m-4 md:mb-2"
+        />
+    {/if}
 
-    <h3 class="text-2xl">{project.name}</h3>
+    <h3 class="text-2xl" class:mr-4={!project.image}>{project.name}</h3>
     <p class="mb-auto">{project.description}</p>
 
     {#if project.tags}
@@ -60,7 +62,10 @@
         </ul>
     {/if}
 
-    <div class="absolute right-2 top-2 flex flex-row-reverse gap-2 text-2xl">
+    <div
+        class="absolute right-0 top-0 flex flex-row-reverse gap-2 rounded-bl-md p-2 text-2xl"
+        class:bg-inherit={project.image}
+    >
         {#if project.repo}
             <a
                 href={project.repo}
