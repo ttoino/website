5545e716a7ce604b90069c22f8c42a63f059dd34
toino <me@toino.pt>
2026-01-12 14:46:39 +0000

    Store user scroll between navigation

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     import type { HTMLAttributes } from "svelte/elements";
 
-    import { fly } from "svelte/transition";
-
     let {
         children,
         class: className = "",
@@ -10,12 +8,6 @@
     }: HTMLAttributes<HTMLDivElement> = $props();
 </script>
 
-<div
-    class="col-span-full row-span-full flex w-full flex-col items-center overflow-x-hidden overflow-y-auto p-4"
-    transition:fly={{ y: 32 }}
-    {...props}
->
-    <div class="w-full {className}">
-        {@render children?.()}
-    </div>
+<div class="w-full {className}" {...props}>
+    {@render children?.()}
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,12 @@
 
     import "./style.css";
 
+    import { onNavigate } from "$app/navigation";
     import { page } from "$app/state";
     import Header from "$lib/components/Header.svelte";
+    import { fly } from "svelte/transition";
+
+    import type { Snapshot } from "./$types";
 
     let {
         children,
@@ -13,6 +17,25 @@
     } = $props();
 
     let index = $derived(page.route.id === "/");
+
+    // Store user scroll between navigation
+    let scrollElement: HTMLDivElement | null = $state(null);
+    let scrollPosition: number = $state(0);
+
+    // Reset scroll before the snapshot is restored
+    onNavigate(() => void (scrollPosition = 0));
+
+    export const snapshot: Snapshot<number | undefined> = {
+        capture: () => scrollElement?.scrollTop,
+        restore: (value) => {
+            if (value !== undefined) scrollPosition = value;
+        },
+    };
+
+    $effect(() => {
+        // eslint-disable-next-line svelte/@typescript-eslint/no-unnecessary-condition
+        if (scrollElement) scrollElement.scrollTop = scrollPosition;
+    });
 </script>
 
 <div
@@ -25,6 +48,14 @@
     <main
         class="grid grid-cols-1 grid-rows-1 justify-items-center overflow-hidden"
     >
-        {@render children?.()}
+        {#key page.url.pathname}
+            <div
+                bind:this={scrollElement}
+                class="col-span-full row-span-full flex w-full flex-col items-center overflow-x-hidden overflow-y-auto p-4"
+                transition:fly={{ y: 32 }}
+            >
+                {@render children?.()}
+            </div>
+        {/key}
     </main>
 </div>
