3372b3c5bb413a0a7849c1bb21ce0b705166cf12
toino <me@toino.pt>
2026-01-23 10:38:17 +0000

    Add about me page

---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Page from "$lib/components/Page.svelte";
+    import Title from "$lib/components/Title.svelte";
+    import { MetaTags } from "svelte-meta-tags";
+</script>
+
+<MetaTags title="About me • toino" />
+
+<Page class="flex max-w-(--breakpoint-xl) flex-col">
+    <Title>About me</Title>
+</Page>
